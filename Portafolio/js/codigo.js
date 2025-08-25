e148bfdead932455346c8b04b32ea6034994a11a
Alejandro <alejandro27ml@gmail.com>
2025-08-25 11:37:19 +0200

    25/0825

---
+++ Portafolio/js/codigo.js
@@ -0,0 +1,30 @@
+/* Menú */
+
+let lanzador = "#enlace-menu";
+let desplegable = "#menu";
+let despliegaClase ="menu-desplegado";
+
+function nav(){
+    let lanz = document.querySelector(lanzador);
+    lanz.addEventListener("click", despliegaMenu);
+}
+
+function despliegaMenu(){
+    let despl = document.querySelector(desplegable);
+    despl.classList.toggle(despliegaClase);
+}
+
+nav();
+
+/* Cookies*/ 
+const popup = document.getElementById("cookiePopup");
+const btn = document.getElementById("acceptCookies");
+
+if(!localStorage.cookiesAccepted){
+    popup.classList.add("show");
+}
+
+btn.addEventListener("click" , function(){
+    localStorage.cookiesAccepted = "true";
+    popup.classList.remove("show");
+})
